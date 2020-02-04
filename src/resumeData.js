54d0ff4e83ca3f95181ef713915ceaa50f356c57
shoaibfy <shoaibfy@gmail.com>
2020-02-04 19:44:08 +0530

    images

---
+++ src/resumeData.js
@@ -0,0 +1,136 @@
+let resumeData = {
+  // "imagebaseurl": "https://rbhatia46.github.io/",
+  "name": "Shoaib Ahmed",
+  "role": "Full Stack Developer",
+  // "linkedinId":"Your LinkedIn Id",
+  "skypeid": "shoaib5023",
+  "roleDescription": "I like  in various parts of frontend development and backend development like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/shoaib-ahmed-s-md-83244a13a",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/Shoaibfy",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "https://www.instagram.com/shoaib_salaf/?hl=en",
+      "className": "fa fa-instagram"
+    }
+  ],
+  "aboutme": "I am currently doing a Full Stack Developer Course at TarzanSkills. I am a self taught Full Stack Web Developer, currently diving deeper into Django framework and Reactjs. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  "address": "Bengaluru    India",
+  "phoneNumber": "8099214545",
+  "email": "shoaib.ahmed.mds@gmail.com",
+  "resume": [
+    {
+      "name": "Resume",
+      "url": "https://github.com/Shoaibfy/calculator/blob/master/shoaib.ahmed.pdf?usp=sharing",
+      "className": "fa fa-file fa-5x"
+    }],
+
+
+  "education": [
+    {
+      "UniversityName": "Geethanjali College of Engineering,Kurnool-A.P",
+      "specialization": "Cad/Cam",
+      "MonthOfPassing": "Oct",
+      "YearOfPassing": "2019",
+
+    },
+    {
+      "UniversityName": "KKRIT College of Engineering,Kurnool-A.P",
+      "specialization": "Mechanical",
+      "MonthOfPassing": "Jun",
+      "YearOfPassing": "2016",
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Real Time Skills Private Limited",
+      "specialization": "Full Stack Engineer in Training",
+      "MonthOfLeaving": "10/2019 - Current",
+
+
+
+
+    },
+  ],
+  "skillsDescription": "",
+  "skills": [
+    {
+      "skillname": "Python"
+    },
+    {
+      "skillname": "DjangoFramework"
+    },
+    {
+      "skillname": "JavaScript"
+    },
+
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Bootstrap"
+    },
+    {
+      "skillname": "MySQL"
+    },
+    {
+      "skillname": "PostgreSQL"
+    },
+
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "redux"
+    },
+    {
+      "skillname": "github"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "Student Details Management",
+      "imgurl": "https://github.com/Shoaibfy/Student-details-management-system/blob/master/image/studentlist.png",
+      // "livedemo": "live demo "
+    },
+
+    {
+      "name": "project2",
+      "description": "Quiz Application",
+      "imgurl": "https://github.com/Shoaibfy/Quiz-Application/blob/master/image/quizapp.png",
+      "url": "https://nandansudoku.netlify.com/",
+     "livedemo": "live demo "
+    },
+    {
+      "name": "project3",
+      "description": "Caluculate product price",
+      "imgurl": "https://github.com/Shoaibfy/calculator/blob/master/calculator/src/productprice.png",
+       "livedemo": "live demo "
+    },
+   
+  ],
+  "testimonials": [
+    {
+      "description": " I am a full stack developer",
+      "name": "Shoaib Ahmed"
+    },
+    {
+      "description": "i am currently working  tarzan",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
